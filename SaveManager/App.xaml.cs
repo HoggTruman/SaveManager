9ac2ac251a09a52b03c5730dc7a20a62059e67c0
Truman Hogg <hoggtruman@gmail.com>
2025-08-05 20:44:24 +0100

    Init

---
+++ SaveManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SaveManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
