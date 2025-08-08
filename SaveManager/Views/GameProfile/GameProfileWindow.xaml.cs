71e898d7ffbbb94d3e0649e62bee20ad6c11f7a2
Truman Hogg <hoggtruman@gmail.com>
2025-08-08 19:53:18 +0100

    Add GameProfileWindow

---
+++ SaveManager/Views/GameProfile/GameProfileWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaveManager.Views.GameProfile
+{
+    /// <summary>
+    /// Interaction logic for GameProfileWindow.xaml
+    /// </summary>
+    public partial class GameProfileWindow : Window
+    {
+        public GameProfileWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
