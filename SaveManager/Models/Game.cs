3f83e02513ba042a2b92dc7fa31e3867e86136d7
Truman Hogg <hoggtruman@gmail.com>
2025-08-11 18:46:30 +0100

    Add Game Model

---
+++ SaveManager/Models/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace SaveManager.Models;
+
+public class Game
+{
+    public required string Name { get; set; }
+    public string SavefileLocation { get; set; } = "";
+    public string ProfilesDirectory { get; set; } = "";
+}
