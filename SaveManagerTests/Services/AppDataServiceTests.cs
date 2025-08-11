2b2d3d39419c23574c5c48622f2cd92e3c39c164
Truman Hogg <hoggtruman@gmail.com>
2025-08-11 19:28:21 +0100

    Add xml conversion method tests

---
+++ SaveManagerTests/Services/AppDataServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using SaveManager.Models;
+using SaveManager.Services;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace SaveManagerTests.Services;
+
+public class AppDataServiceTests
+{
+    #region ConvertToXElement Tests
+
+    [Fact]
+    public void ConvertToXElement_WithNull_ThrowsArgumentNullException()
+    {
+        string nullObject = null!;
+        Assert.Throws<ArgumentNullException>(() => AppdataService.ConvertToXElement(nullObject));
+    }
+
+    [Fact]
+    public void ConvertToXElement_ConvertsGame()
+    {
+        Game game = new() { Name = "test game", ProfilesDirectory = "test directory", SavefileLocation = "test savefilelocation" };
+
+        XElement gameElement = AppdataService.ConvertToXElement(game);
+
+        Assert.Equal(game.GetType().Name, gameElement.Name);
+
+        foreach (XElement element in gameElement.Elements())
+        {
+            string propertyName = element.Name.LocalName;
+            PropertyInfo? prop = typeof(Game).GetProperty(propertyName);
+            Assert.NotNull(prop);
+            Assert.Equal(prop.GetValue(game), element.Value);
+        }
+    }
+
+    #endregion
+
+
+    #region ConvertFromXElement Tests
+
+    [Fact]
+    public void ConvertFromXElement_WithNull_ThrowsArgumentNullException()
+    {
+        XElement nullObject = null!;
+        Assert.Throws<ArgumentNullException>(() => AppdataService.ConvertFromXElement<string>(nullObject));
+    }
+
+    [Fact]
+    public void ConvertFromXElement_WithGameElement_ReturnsGame()
+    {
+        Game expectedGame = new() { Name = "test game", ProfilesDirectory = "test directory", SavefileLocation = "test savefilelocation" };
+        var children = typeof(Game).GetProperties().Select(prop => new XElement(prop.Name, prop.GetValue(expectedGame)));
+
+        XElement gameElement = new(expectedGame.GetType().Name, children);
+        Game result = AppdataService.ConvertFromXElement<Game>(gameElement);
+
+        Assert.Equivalent(expectedGame, result, true);
+    }
+
+    #endregion
+}
