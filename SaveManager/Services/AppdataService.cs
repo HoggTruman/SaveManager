3cca8a4dc5ee4fc3a7982fd489519f0e86be856c
Truman Hogg <hoggtruman@gmail.com>
2025-08-12 20:20:43 +0100

    Add Game methods

--- SaveManager/Services/AppdataService.cs
+++ SaveManager/Services/AppdataService.cs
@@ -1,18 +1,23 @@
-﻿using System.IO;
+﻿using SaveManager.Models;
+using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using System.Xml.XPath;
 
 
 namespace SaveManager.Services;
 
 public class AppdataService
 {
+    internal const string GamesName = "Games";
+    internal const string SettingsName = "Settings";
     private const string AppdataDirectoryName = "Save Manager";
     private static readonly string AppdataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppdataDirectoryName);
     private static readonly string AppdataPath = Path.Combine(AppdataFolder, "appdata.xml");
 
-    private XElement _tree = new("Appdata", new XElement("Games"), new XElement("Settings"));
+    private XElement _tree = new("Appdata", new XElement(GamesName), new XElement(SettingsName));
+
 
     /// <summary>
     /// Ensures the appdata file exists and loads it as an XElement.
@@ -36,6 +41,81 @@ public class AppdataService
     }
 
 
+
+    /// <summary>
+    /// Adds a new game to the appdata file.
+    /// </summary>
+    /// <param name="game"></param> 
+    /// <exception cref="IOException"/>
+    public void AddGame(Game game)
+    {
+        XElement newTree = new(_tree);
+        AddGameToTree(newTree, game);
+        _tree.Save(AppdataPath);
+        _tree = newTree;
+    }
+
+
+    /// <summary>
+    /// Renames a game in the appdata file.
+    /// </summary>
+    /// <param name="game"></param>
+    /// <param name="newName"></param>
+    /// <exception cref="IOException"/>
+    public void RenameGame(Game game, string newName)
+    {
+        XElement newTree = new(_tree);
+        RenameGameInTree(newTree, game, newName);
+        _tree.Save(AppdataPath);
+        _tree = newTree;
+    }
+
+
+    /// <summary>
+    /// Deletes a game in the appdata file.
+    /// </summary>
+    /// <param name="game"></param>
+    /// <exception cref="IOException"/>
+    public void DeleteGame(Game game)
+    {
+        XElement newTree = new(_tree);
+        DeleteGameInTree(newTree, game);
+        _tree.Save(AppdataPath);
+        _tree = newTree;
+    }
+
+
+
+
+    internal static void AddGameToTree(XElement tree, Game game)
+    {
+        XElement gamesElement = tree.Elements().First(x => x.Name.LocalName == GamesName);
+        gamesElement.Add(ConvertToXElement(game));
+    }
+
+
+    internal static void RenameGameInTree(XElement tree, Game game, string newName)
+    {
+        XElement nameElement = tree.Elements()
+            .First(x => x.Name.LocalName == GamesName)
+            .Descendants()
+            .First(x => x.Name.LocalName == nameof(Game.Name) && x.Value == game.Name);
+
+        nameElement.Value = newName;
+    }
+
+
+    internal static void DeleteGameInTree(XElement tree, Game game)
+    {
+        XElement gameElement = tree.Elements()
+            .First(x => x.Name.LocalName == GamesName)
+            .Elements()
+            .First(x => x.Elements().First(y => y.Name.LocalName == nameof(Game.Name)).Value == game.Name);
+
+        gameElement.Remove();
+    }
+
+
     internal static XElement ConvertToXElement<T>(T item) where T : notnull
     {
         if (item == null)
@@ -52,6 +132,7 @@ public class AppdataService
 
         return doc.Root!;
     }
+
 
     internal static T ConvertFromXElement<T>(XElement element) where T : notnull
     {
