963accd6ac2d0bcb9879193a415e90b8a6e9de8f
Truman Hogg <hoggtruman@gmail.com>
2025-08-14 23:34:11 +0100

    Move controls directly into SaveWindow

--- SaveManager/Views/Save/SaveWindow.xaml.cs
+++ SaveManager/Views/Save/SaveWindow.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SaveManager.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace SaveManager.Views.Save
+namespace SaveManager.Views.Save;
+
+/// <summary>
+/// Interaction logic for SaveWindow.xaml
+/// </summary>
+public partial class SaveWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for SaveWindow.xaml
-    /// </summary>
-    public partial class SaveWindow : Window
+    public SaveWindow()
     {
-        public SaveWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
