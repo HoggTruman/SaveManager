089c9a45b8caf1306b0cfaea650a182285692119
Truman Hogg <hoggtruman@gmail.com>
2025-08-18 22:45:50 +0100

    Add ability to access GameProfileWindow

--- SaveManager/Views/Save/SaveWindow.xaml.cs
+++ SaveManager/Views/Save/SaveWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using SaveManager.ViewModels;
+﻿using SaveManager.Services;
+using SaveManager.ViewModels;
+using SaveManager.Views.GameProfile;
 using System.Windows;
 
 namespace SaveManager.Views.Save;
@@ -8,8 +10,16 @@ namespace SaveManager.Views.Save;
 /// </summary>
 public partial class SaveWindow : Window
 {
+    public SaveViewModel SaveViewModel { get; } = new(new AppdataService());
+
     public SaveWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        GameProfileWindow gameProfileWindow = new();
+        gameProfileWindow.ShowDialog();
     }
 }
