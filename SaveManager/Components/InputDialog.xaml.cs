02573cd9bc9169c64e39fdea1d3bceef3983af47
Truman Hogg <hoggtruman@gmail.com>
2025-08-19 01:20:10 +0100

    Add InputDialog

---
+++ SaveManager/Components/InputDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace SaveManager.Components;
+
+/// <summary>
+/// Interaction logic for InputDialog.xaml
+/// </summary>
+public partial class InputDialog : Window
+{
+    public string Input => InputTextBox.Text;
+
+    public InputDialog(string title, string prompt, string startingInput="")
+    {
+        InitializeComponent();
+        Title = title;
+        PromptLabel.Content = prompt;
+        InputTextBox.Text = startingInput;
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+
+    private void Window_ContentRendered(object sender, EventArgs e)
+    {
+        InputTextBox.SelectAll();
+		InputTextBox.Focus();
+    }
+}
