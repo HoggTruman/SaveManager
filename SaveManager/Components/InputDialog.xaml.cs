896d204ec4863617fbed61e8f0a85f9f2ffc1483
Truman Hogg <hoggtruman@gmail.com>
2025-08-19 19:44:52 +0100

    Update InputDialog with validator

--- SaveManager/Components/InputDialog.xaml.cs
+++ SaveManager/Components/InputDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using SaveManager.Validators;
+using System.Windows;
 
 namespace SaveManager.Components;
 
@@ -7,19 +8,29 @@ namespace SaveManager.Components;
 /// </summary>
 public partial class InputDialog : Window
 {
+    private readonly IValidator? _validator;
+
     public string Input => InputTextBox.Text;
 
-    public InputDialog(string title, string prompt, string startingInput="")
+    public InputDialog(string title, string prompt, int maxLength=0, string startingInput="", IValidator? validator=null)
     {
         InitializeComponent();
         Title = title;
         PromptLabel.Content = prompt;
+        InputTextBox.MaxLength = maxLength;
         InputTextBox.Text = startingInput;
+        _validator = validator;
     }
 
     private void OkButton_Click(object sender, RoutedEventArgs e)
     {
-        DialogResult = true;
+        if (_validator != null && !_validator.IsValid(Input))
+        {
+            MessageBox.Show(_validator.Message);
+            return;
+        }
+
+        DialogResult = true;             
     }
 
     private void Window_ContentRendered(object sender, EventArgs e)
