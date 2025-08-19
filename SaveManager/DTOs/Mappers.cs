c83212b12d993d876cb099093a515df75b21ba10
Truman Hogg <hoggtruman@gmail.com>
2025-08-19 17:53:49 +0100

    Add Mappers

---
+++ SaveManager/DTOs/Mappers.cs
@@ -0,0 +1,16 @@
+﻿using SaveManager.Models;
+
+namespace SaveManager.DTOs;
+
+public static class Mappers
+{
+    public static GameAppdataDTO ToGameAppdataDTO(this Game game)
+    {
+        return new()
+        {
+            Name = game.Name,
+            ProfilesDirectory = game.ProfilesDirectory,
+            SavefileLocation = game.SavefileLocation
+        };
+    }
+}
