df8e61eda33da6db460eee36a55396c5ee67c6a4
Truman Hogg <hoggtruman@gmail.com>
2025-08-19 01:35:48 +0100

    Add GameAppdataDTO

---
+++ SaveManager/DTOs/GameAppdataDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace SaveManager.DTOs;
+
+[XmlType(TypeName = "Game")]
+public class GameAppdataDTO
+{
+    public required string Name { get; set; }
+    public string? SavefileLocation { get; set; }
+    public string? ProfilesDirectory { get; set; }    
+}
