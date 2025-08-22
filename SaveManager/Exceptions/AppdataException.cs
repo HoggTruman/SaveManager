0763c7aa2c5d459f188a9dfc890b4988369a2d01
Truman Hogg <hoggtruman@gmail.com>
2025-08-22 22:23:28 +0100

    Add AppdataException

---
+++ SaveManager/Exceptions/AppdataException.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveManager.Exceptions;
+
+public class AppdataException : Exception
+{
+    public AppdataException() { }
+
+    public AppdataException(string? message) : base(message) { }
+
+    public AppdataException(string? message, Exception? innerException) : base (message, innerException) { }    
+}
