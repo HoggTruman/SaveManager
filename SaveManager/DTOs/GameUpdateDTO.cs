b57f90ae241bbbc9220478e578f0d87cd59e8e75
Truman Hogg <hoggtruman@gmail.com>
2025-08-27 00:14:04 +0100

    Add GameUpdateDTO

---
+++ SaveManager/DTOs/GameUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SaveManager.DTOs;
+
+public class GameUpdateDTO
+{
+    public string? Name { get; set; }
+    public string? SavefileLocation { get; set; }
+    public string? ProfilesDirectory { get; set; }    
+}
