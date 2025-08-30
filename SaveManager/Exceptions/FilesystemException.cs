b80d83d6c267526313c7827723320fd30a5fabce
Truman Hogg <hoggtruman@gmail.com>
2025-08-30 02:08:35 +0100

    Add custom exceptions

---
+++ SaveManager/Exceptions/FilesystemException.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveManager.Exceptions;
+
+public class FilesystemException : Exception
+{
+    public FilesystemException() { }
+
+    public FilesystemException(string? message) : base(message) { }
+
+    public FilesystemException(string? message, Exception? innerException) : base (message, innerException) { } 
+}
---
+++ SaveManager/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveManager.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException() { }
+
+    public ValidationException(string? message) : base(message) { }
+
+    public ValidationException(string? message, Exception? innerException) : base (message, innerException) { }    
+}
