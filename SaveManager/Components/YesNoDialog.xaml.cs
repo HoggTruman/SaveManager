6f7793a2829803b69e6d914f252eb1d5f3dca52f
Truman Hogg <hoggtruman@gmail.com>
2025-09-02 01:53:33 +0100

    Add YesNoDialog

---
+++ SaveManager/Components/YesNoDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace SaveManager.Components;
+
+/// <summary>
+/// Interaction logic for YesNoDialog.xaml
+/// </summary>
+public partial class YesNoDialog : Window
+{
+    public string Prompt => PromptLabel.Content.ToString()!;
+
+    public YesNoDialog(string title, string prompt)
+    {
+        InitializeComponent();
+        Title = title;
+        PromptLabel.Content = prompt;
+    }
+
+    private void YesButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;             
+    }
+}
