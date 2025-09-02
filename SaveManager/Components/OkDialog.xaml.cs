669f4cbdae29265ad5e003bd58ad5e47c28be35e
Truman Hogg <hoggtruman@gmail.com>
2025-09-02 02:27:09 +0100

    Add OkDialog

---
+++ SaveManager/Components/OkDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace SaveManager.Components;
+
+/// <summary>
+/// Interaction logic for OkDialog.xaml
+/// </summary>
+public partial class OkDialog : Window
+{
+    public string Prompt => PromptLabel.Content.ToString()!;
+
+    public OkDialog(string title, string prompt)
+    {
+        InitializeComponent();
+        Title = title;
+        PromptLabel.Content = prompt;
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;             
+    }
+}
