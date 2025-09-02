3ba713707cb7cd25364a40f633fa485d70cc459d
Truman Hogg <hoggtruman@gmail.com>
2025-09-02 03:22:36 +0100

    Clean up docs

--- SaveManager/Extensions/WindowExtensions.cs
+++ SaveManager/Extensions/WindowExtensions.cs
@@ -4,13 +4,10 @@ namespace SaveManager.Extensions;
 
 public static class WindowExtensions
 {
-    /// <summary>
-    /// Opens a window and returns only when the newly opened window is closed.
-    /// </summary>
+    
+    /// <inheritdoc cref="Window.ShowDialog()"></inheritdoc>
     /// <param name="window">The window to show.</param>
     /// <param name="owner">The owner of the window</param>
-    /// <returns>A <see cref="Nullable{T}"/> of type <see cref="Boolean"/> that specifies whether the activity was accepted (true) or canceled (false).</returns>
-    /// <exception cref="InvalidOperationException"></exception>
     public static bool? ShowDialog(this Window window, Window owner)
     {
         window.Owner = owner;
