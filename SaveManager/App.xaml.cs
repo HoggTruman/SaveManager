d8156abbd27da5e83f54222bf9528cd43c46ad07
Truman Hogg <hoggtruman@gmail.com>
2025-09-03 06:31:16 +0100

    Update app startup

--- SaveManager/App.xaml.cs
+++ SaveManager/App.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using SaveManager.ViewModels;
+using SaveManager.Views.Save;
 using System.Windows;
 
-namespace SaveManager
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace SaveManager;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);    
+        SaveWindow mainWindow = new(ViewModelFactory.CreateSaveViewModel());
+        mainWindow.Show();            
+    }
 }
