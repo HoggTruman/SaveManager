6672fb994a4af1e81871ff98c8aa104724a23672
Truman Hogg <hoggtruman@gmail.com>
2025-09-03 06:30:22 +0100

    Update IFilesystemItem

--- SaveManager/Models/IFilesystemItem.cs
+++ SaveManager/Models/IFilesystemItem.cs
@@ -1,7 +1,26 @@
-﻿namespace SaveManager.Models;
+﻿using SaveManager.Exceptions;
+
+namespace SaveManager.Models;
 
 public interface IFilesystemItem
 {
+    /// <summary>
+    /// The full filesystem path of the underlying file / directory.
+    /// </summary>
     public string Location { get; }
+
+    /// <summary>
+    /// The name of the underlying file / directory.
+    /// </summary>
     public string Name { get; }
+
+
+    /// <summary>
+    /// Updates the IFilesystemItem's location for the internal filesystem representation.
+    /// Does not actually affect any files or directories.
+    /// </summary>
+    /// <param name="newLocation"></param>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="ValidationException"></exception>
+    public void UpdateLocation(string newLocation);
 }
