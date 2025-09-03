759cca5b5b1117b30e89776b14cc85e14f5a8d79
Truman Hogg <hoggtruman@gmail.com>
2025-09-03 08:47:02 +0100

    Fix name

--- SaveManagerTests/Services/AppdataServiceTests.cs
+++ SaveManagerTests/Services/AppdataServiceTests.cs
@@ -10,7 +10,48 @@ public class AppdataServiceTests
 {
     #region Tree Modification Tests (USES CONVERSION METHODS)
 
-    
+    [Fact]
+    public void AddGameToDocument_AddsGameElement()
+    {
+        XDocument document = AppdataService.DefaultDocument;
+        Game game = new("test", "savefile", "profiles");
+        AppdataService.AddGameToDocument(document, game);
+        var gameElements = document.Root!.Element(AppdataService.GamesName)!.Elements();        
+
+        Assert.Equal(2, document.Root.Elements().Count());
+        Assert.Single(gameElements);
+    }
+
+    [Fact]
+    public void UpdateGameInDocument_RenamesGameElement()
+    {
+        XDocument document = AppdataService.DefaultDocument;
+        Game game = new("test", "savefile", "profiles");
+        GameAppdataDTO gameDTO = game.ToGameAppdataDTO();
+        var gamesElement = document.Root!.Element(AppdataService.GamesName)!;
+        gamesElement.Add(AppdataService.ConvertToXElement(gameDTO));
+        GameUpdateDTO updateDTO = new(){ Name = "new name" };
+
+        AppdataService.UpdateGameInDocument(document, game, updateDTO);
+
+        XElement gameElement = gamesElement.Elements().First();
+        XElement nameElement = gameElement.Element(nameof(Game.Name))!;
+        Assert.Equal(updateDTO.Name, nameElement.Value);
+    }
+
+    [Fact]
+    public void DeleteGameInDocument_DeletesGameElement()
+    {
+        XDocument document = AppdataService.DefaultDocument;
+        Game game = new("test", "savefile", "profiles");
+        GameAppdataDTO gameDTO = game.ToGameAppdataDTO();
+        var gamesElement = document.Root!.Element(AppdataService.GamesName)!;
+        gamesElement.Add(AppdataService.ConvertToXElement(gameDTO));
+
+        AppdataService.DeleteGameInDocument(document, game);
+
+        Assert.Empty(gamesElement.Elements());
+    }
 
     #endregion
 
