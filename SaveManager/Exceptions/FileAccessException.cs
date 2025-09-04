10eeeeb5d8740003e22d6028b638f8cb4e179e91
Truman Hogg <hoggtruman@gmail.com>
2025-09-04 16:35:53 +0100

    Add FileAccessException

---
+++ SaveManager/Exceptions/FileAccessException.cs
@@ -0,0 +1,13 @@
+﻿namespace SaveManager.Exceptions;
+
+/// <summary>
+/// An exception thrown for file access related errors such as <see cref="UnauthorizedAccessException"/> and <see cref="System.Security.SecurityException"/>
+/// </summary>
+public class FileAccessException : Exception
+{
+    public FileAccessException() { }
+
+    public FileAccessException(string? message) : base(message) { }
+
+    public FileAccessException(string? message, Exception? innerException) : base (message, innerException) { }   
+}
