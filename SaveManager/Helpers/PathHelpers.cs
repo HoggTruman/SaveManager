e8875fcda614020ed692c9cb3617fe8e550671f8
Truman Hogg <hoggtruman@gmail.com>
2025-09-05 15:21:57 +0100

    Add PathHelpers

---
+++ SaveManager/Helpers/PathHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace SaveManager.Helpers;
+
+public static class PathHelpers
+{
+    /// <summary>
+    /// Returns true if neither path is the parent of the other, otherwise false.
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
+    public static bool AreParentChildDirectories(string first, string second)
+    {
+        if (first == second)
+            return false;
+
+        Uri firstUri = new(first.EndsWith(Path.DirectorySeparatorChar) ? first : first + Path.DirectorySeparatorChar);
+        Uri secondUri = new(second.EndsWith(Path.DirectorySeparatorChar) ? second : second + Path.DirectorySeparatorChar);
+        return firstUri.IsBaseOf(secondUri) || secondUri.IsBaseOf(firstUri);
+    }
+}
---
+++ SaveManagerTests/Helpers/PathHelpersTests.cs
@@ -0,0 +1,43 @@
+﻿using SaveManager.Helpers;
+
+namespace SaveManagerTests.Helpers;
+
+public class PathHelpersTests
+{
+    [Fact]
+    public void AreParentChildDirectories_ReturnsTrue_WhenParentAndChild()
+    {
+        string parent = Path.Join("C:","Parent");
+        string child = Path.Join(parent, "Child");
+        Assert.True(PathHelpers.AreParentChildDirectories(parent, child));
+        Assert.True(PathHelpers.AreParentChildDirectories(child, parent));
+    }
+
+
+    [Fact]
+    public void AreParentChildDirectories_ReturnsFalse_WhenSiblings()
+    {
+        string first = @"C:\somerandomdir";
+        string second = @"C:\someotherdir";
+        Assert.False(PathHelpers.AreParentChildDirectories(first, second));
+        Assert.False(PathHelpers.AreParentChildDirectories(second, first));
+    }
+
+
+    [Fact]
+    public void AreParentChildDirectories_ReturnsFalse_WhenNotParentChild()
+    {
+        string first = Path.Join("C:","a", "b");
+        string second = Path.Join("C:","b", "f", "g");
+        Assert.False(PathHelpers.AreParentChildDirectories(first, second));
+        Assert.False(PathHelpers.AreParentChildDirectories(second, first));
+    }
+
+
+    [Fact]
+    public void AreParentChildDirectories_ReturnsFalse_WhenGivenSamePath()
+    {
+        string path = Path.Join("C:", "Folder");
+        Assert.False(PathHelpers.AreParentChildDirectories(path, path));
+    }    
+}
