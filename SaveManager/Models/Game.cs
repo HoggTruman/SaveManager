3ec9be9f3ade732928a5c145d0187a308042f6a0
Truman Hogg <hoggtruman@gmail.com>
2025-09-08 13:46:17 +0100

    Update Game / Profile

--- SaveManager/Models/Game.cs
+++ SaveManager/Models/Game.cs
@@ -1,26 +1,173 @@
-﻿using SaveManager.DTOs;
-using System.IO;
+﻿using SaveManager.Exceptions;
+using SaveManager.ViewModels;
+using System.Collections.ObjectModel;
 
 namespace SaveManager.Models;
 
-public class Game
+public class Game : NotifyPropertyChanged
 {
-    public required string Name { get; set; }
-    public string? SavefileLocation { get; set; }
-    public string? ProfilesDirectory { get; set; }
-
-    public List<Profile> Profiles { get; set; } = [];
+    private string _name = "";
+    private string? _savefileLocation;
+    private Folder? _profilesFolder;
+    private ObservableCollection<Profile> _profiles = [];
+    
 
 
     /// <summary>
-    /// Loads the games profiles from the filesystem.
+    /// The name of the game.
     /// </summary>
-    /// <exception cref="UnauthorizedAccessException"/>
-    /// <exception cref="System.Security.SecurityException"/>
-    public void LoadProfiles()
+    /// <exception cref="ValidationException"></exception>
+    public string Name
+    { 
+        get => _name; 
+        set
+        {
+            ValidateName(value);
+            SetProperty(ref _name, value);
+        }
+    }
+
+    /// <summary>
+    /// The full path of the savefile.
+    /// </summary>
+    public string? SavefileLocation
+    { 
+        get => _savefileLocation; 
+        set => SetProperty(ref _savefileLocation, value); 
+    }
+
+
+    /// <summary>
+    /// The full path of the directory containing profile directories.
+    /// </summary>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public string? ProfilesDirectory
+    { 
+        get => _profilesFolder?.Location; 
+        set
+        {
+            if (value == null)
+            {
+                SetProperty(ref _profilesFolder, null);
+                Profiles = [];
+            }
+            else
+            {
+                try
+                {
+                    Folder newFolder = new(value, null);
+                    SetProperty(ref _profilesFolder, newFolder);
+                    Profiles = [..newFolder.Children.Where(x => x is Folder).Select(x => new Profile((Folder)x, this))];
+                }
+                catch (Exception ex)
+                {
+                    // If profiles directory can not be accessed, remove it to prevent locking app.
+                    if (ex is FileAccessException or FilesystemException)
+                    {
+                        SetProperty(ref _profilesFolder, null);
+                        Profiles = [];
+                    }
+                    
+                    throw;
+                }
+            }
+        }
+    }
+
+    public ObservableCollection<Profile> Profiles 
+    { 
+        get => _profiles; 
+        set => SetProperty(ref _profiles, value); 
+    }
+
+
+
+
+
+    /// <summary>
+    /// Initializes a new <see cref="Game"/> instance. 
+    /// Automatically loads its profiles from the filesystem.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="savefileLocation"></param>
+    /// <param name="profilesDirectory"></param>
+    /// <exception cref="ValidationException">An invalid name is provided.</exception>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public Game(string name, string? savefileLocation=null, string? profilesDirectory=null)
     {
-        Profiles = ProfilesDirectory != null && Directory.Exists(ProfilesDirectory)?
-            [..Directory.EnumerateDirectories(ProfilesDirectory).Select(x => new Profile(x))]:
-            [];
+        Name = name;
+        SavefileLocation = savefileLocation;
+        try
+        {
+            ProfilesDirectory = profilesDirectory;
+        }
+        catch (Exception ex)
+        {
+            // Games are only constructed with a profilesDirectory on app startup.
+            // If the directory is no longer accessible, it will be set to null in ProfilesDirectory setter and we can continue.
+            if (ex is FilesystemException or FileAccessException)
+                return;
+
+            throw;
+        }        
+    }
+
+
+    /// <summary>
+    /// Creates a new profile in the game's profiles directory.
+    /// Returns a <see cref="Profile"/> instance representing it.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <exception cref="ValidationException"></exception>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public Profile CreateProfile(string name)
+    {
+        if (ProfilesDirectory == null || _profilesFolder == null)
+        {
+            throw new InvalidOperationException("ProfilesDirectory must be set before a profile can be created");
+        }
+
+        if (Profiles.Any(x => x.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            throw new ValidationException($"A profile already exists with name: {name}");
+        }
+
+        Folder profileFolder = Folder.Create(name, _profilesFolder);
+        Profile profile = new(profileFolder, this);
+        _profiles.Add(profile);
+        SortProfiles();
+        return profile;
+    }
+
+
+
+
+    /// <summary>
+    /// Reloads the game's profiles from the filesystem.
+    /// </summary>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public void RefreshProfiles()
+    {
+        ProfilesDirectory = _profilesFolder?.Location;
+    }
+
+
+    /// <summary>
+    /// Sorts profiles by name.
+    /// </summary>
+    public void SortProfiles()
+    {
+        Profiles = [..Profiles.OrderBy(x => x.Name)];
+    }
+
+
+    private static void ValidateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ValidationException("Game name can not be empty or whitespace.");
     }
 }
--- SaveManager/Models/Profile.cs
+++ SaveManager/Models/Profile.cs
@@ -1,9 +1,54 @@
-﻿using System.IO;
+﻿using SaveManager.Exceptions;
 
 namespace SaveManager.Models;
 
-public class Profile : Folder
+public class Profile
 {
-    public Profile(string location) : base(location) { }
-    public Profile(DirectoryInfo directoryInfo) : base(directoryInfo) { }
+    public Game Game { get; }
+    public Folder Folder { get; }
+
+    public string Name => Folder.Name;
+
+
+    /// <summary>
+    /// Initializes a new <see cref="Profile"/> instance.
+    /// </summary>
+    /// <param name="folder"></param>
+    /// <param name="game"></param>
+    public Profile(Folder folder, Game game)
+    {
+        Folder = folder;
+        Game = game;
+    }
+
+
+    /// <summary>
+    /// Renames the Profile.
+    /// </summary>
+    /// <param name="newName"></param>
+    /// <exception cref="ValidationException"></exception>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public void Rename(string newName)
+    {
+        if (Game.Profiles.Any(x => x.Name.Equals(newName, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            throw new ValidationException("A Profile already exists with this name");
+        }
+
+        Folder.Rename(newName);
+        Game.SortProfiles();
+    }
+
+
+    /// <summary>
+    /// Deletes the <see cref="Profile"/>.
+    /// </summary>
+    /// <exception cref="FilesystemException"></exception>
+    /// <exception cref="FileAccessException"></exception>
+    public void Delete()
+    {
+        Folder.Delete();
+        Game.Profiles = [..Game.Profiles.Where(x => x != this)];
+    }
 }
