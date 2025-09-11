ff6ecc78037dbb1ad04d79502a8b1862634c04f2
Truman Hogg <hoggtruman@gmail.com>
2025-09-11 13:23:36 +0100

    Add FileTests

---
+++ SaveManagerTests/Models/FileTests.cs
@@ -0,0 +1,13 @@
+﻿namespace SaveManagerTests.Models;
+
+public class FileTests
+{
+    [Fact]
+    public void UpdateLocation_UpdatesLocation()
+    {
+        SaveManager.Models.File file = new(Path.Join(Directory.GetCurrentDirectory(), "file.file"), null);
+        string newLocation = Path.Join(Directory.GetCurrentDirectory(), "newlocation.file");
+        file.UpdateLocation(newLocation);
+        Assert.Equal(newLocation, file.Location);
+    }
+}
