cf1d167ce9a2b20ebf4ab4739ce36f05c3f7dd7c
Truman Hogg <hoggtruman@gmail.com>
2025-09-23 13:33:07 +0100

    Clean up app startup

--- SaveManager/App.xaml.cs
+++ SaveManager/App.xaml.cs
@@ -20,50 +20,60 @@ public partial class App : Application
         {
             AppdataService appdataService = new();
             ViewModelFactory.Initialize(appdataService);
-            List<Game> games = [];
-            int invalidGameCount = 0;
-
-            foreach (GameDTO gameDTO in appdataService.GetGameData())
-            {
-                try
-                {
-                    Game game = new(gameDTO.Name) { SavefileLocation = gameDTO.SavefileLocation };
-                    try
-                    {
-                        game.ProfilesDirectory = gameDTO.ProfilesDirectory;
-                    }
-                    catch (FilesystemItemNotFoundException)
-                    {
-                        // Profiles directory no longer exists so leave it null.
-                    }
-                    catch (FilesystemException)
-                    {
-                        MessageBox.Show("Failed to initialize app.");
-                        return;
-                    }
-
-                    games.Add(game);
-                }
-                catch (ValidationException)
-                {
-                    // Invalid game name retrieved from appdata.xml
-                    ++invalidGameCount;
-                }
-            }
+            List<Game> games = LoadGames(appdataService);            
 
             // Save games once they have been loaded to remove any invalid/outdated data.
-            appdataService.ReplaceGames(games); 
-
-            if (invalidGameCount > 0)
-                MessageBox.Show($"{invalidGameCount} games removed due to invalid data.");
-
+            appdataService.ReplaceGames(games);
             SaveWindow mainWindow = new(ViewModelFactory.CreateSaveViewModel(games));
             mainWindow.Show();  
+        }
+        catch (FilesystemException)
+        {
+            MessageBox.Show("Failed to initialize app.");
         }
         catch (AppdataException ex)
         {
             MessageBox.Show(ex.Message);
+        }        
+    }
+
+    /// <summary>
+    /// Retrieves the user's games from the appdata service and loads their profiles from the filesystem.
+    /// </summary>
+    /// <param name="appdataService"></param>
+    /// <returns>A list of loaded games</returns>
+    /// <exception cref="FilesystemException"></exception>
+    internal List<Game> LoadGames(AppdataService appdataService)
+    {
+        List<Game> games = [];
+        int invalidGameCount = 0;
+
+        foreach (GameDTO gameDTO in appdataService.GetGameData())
+        {
+            try
+            {
+                Game game = new(gameDTO.Name) { SavefileLocation = gameDTO.SavefileLocation };
+                try
+                {
+                    game.ProfilesDirectory = gameDTO.ProfilesDirectory;
+                    games.Add(game);
+                }
+                catch (FilesystemItemNotFoundException)
+                {
+                    // Profiles directory no longer exists
+                    game.ProfilesDirectory = null; 
+                }                
+            }
+            catch (ValidationException)
+            {
+                // Invalid game name retrieved from appdata.xml
+                ++invalidGameCount;
+            }
         }
-        
+
+        if (invalidGameCount > 0)
+                MessageBox.Show($"{invalidGameCount} games removed due to invalid data.");
+
+        return games;
     }
 }
