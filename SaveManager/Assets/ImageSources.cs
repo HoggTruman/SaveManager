9d1b07dc96263e97b6b13cd7aa70975ab3884ea7
Truman Hogg <hoggtruman@gmail.com>
2025-10-02 16:40:46 +0100

    Fix warning image source

--- SaveManager/Assets/ImageSources.cs
+++ SaveManager/Assets/ImageSources.cs
@@ -7,5 +7,5 @@ public static class ImageSources
     public static BitmapImage Error { get; } = new(new Uri(@"/SaveManager;component/Assets/Error.png", UriKind.Relative));
     public static BitmapImage Question { get; } = new(new Uri(@"/SaveManager;component/Assets/Question.png", UriKind.Relative));
     public static BitmapImage Success { get; } = new(new Uri(@"/SaveManager;component/Assets/Success.png", UriKind.Relative));
-    public static BitmapImage Warning { get; } = new(new Uri(@"/SaveManager;component/Assets/Error.png", UriKind.Relative));
+    public static BitmapImage Warning { get; } = new(new Uri(@"/SaveManager;component/Assets/Warning.png", UriKind.Relative));
 }
