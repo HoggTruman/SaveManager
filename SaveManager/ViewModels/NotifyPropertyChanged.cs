8a8dfb52f97e8ad00cb3f2ecce14342db685d7c7
Truman Hogg <hoggtruman@gmail.com>
2025-10-31 19:48:09 +0000

    Simplify SetProperty

--- SaveManager/ViewModels/NotifyPropertyChanged.cs
+++ SaveManager/ViewModels/NotifyPropertyChanged.cs
@@ -12,15 +12,9 @@ public abstract class NotifyPropertyChanged : INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName=null)
+    protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName=null)
     {
-        if (Equals(field, value))
-        {
-            return false;
-        }
-
         field = value;
         OnPropertyChanged(propertyName);
-        return true;
     }
 }
