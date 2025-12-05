49fd1ad0a5e2cfe971dc74cbb9433679dbcad5b4
Truman Hogg <hoggtruman@gmail.com>
2025-12-05 20:58:08 +0000

    Add HotkeyActionId

---
+++ SaveManager/Services/Hotkey/HotkeyActionId.cs
@@ -0,0 +1,11 @@
+﻿namespace SaveManager.Services.Hotkey;
+
+/// <summary>
+/// An enum containing the ID for each action triggerable by a hotkey.
+/// </summary>
+public enum HotkeyActionId
+{
+    ToggleGlobalHotkeys = 9000,
+    LoadSave,
+    ImportSave,    
+}
