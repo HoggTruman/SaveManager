d028b77dd2c5562ec6350c70956eca1f843949c4
Truman Hogg <hoggtruman@gmail.com>
2025-12-19 20:40:55 +0000

    Add Hotkey

---
+++ SaveManager/Services/Hotkey/Hotkey.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows.Input;
+
+namespace SaveManager.Services.Hotkey;
+
+/// <summary>
+/// A record containing the key combination registered as a hotkey.
+/// </summary>
+public record Hotkey
+{
+    public required Key Key { get; init; }
+    public required ModifierKeys ModifierKeys { get; init; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        if (ModifierKeys.HasFlag(ModifierKeys.Control))
+            sb.Append("Ctrl + ");
+        if (ModifierKeys.HasFlag(ModifierKeys.Shift))
+            sb.Append("Shift + ");
+        if (ModifierKeys.HasFlag(ModifierKeys.Alt))
+            sb.Append("Alt + ");
+        if (ModifierKeys.HasFlag(ModifierKeys.Windows))
+            sb.Append("Win + ");
+
+        sb.Append(Key);
+
+        return sb.ToString();
+    }
+}
